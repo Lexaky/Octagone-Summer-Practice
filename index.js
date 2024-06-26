cd4a3b07ad9b4625f877aca7336f6739c92300ea
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-06-26 16:27:28 +1000

    Обновлено решение в index.js: localhost:3000/static возвращает json ответ с header:Hello, body:Octagon NodeJS Test;; localhost:3000/dynamic?abc возвращает результат в json с подстановкой значений a,b,c в формулу (a*b*c)/3

--- index.js
+++ index.js
@@ -3,12 +3,49 @@ const express = require('express');
 const app = express();
 const port = 3000; //открытый порт для подключения к localhost
 
-//Маршрут для корня '/', возврат сервером сообщения
+//Проверка на то, что пришло какое-то число (не NaN и после трима не пустое)
+const isNumber = (value) => { 
+    return !isNaN(value) && value.trim() !== '';
+};
+
+//Маршрут для корня '/' (1-ое задание)
 app.get('/', (req, res) => {
   res.send('<h1>Привет, Октагон!</h1>');
 });
 
-//Запуск сервера на порту port
+//static возвращает json с "Hello" в Header и "Octagon NodeJS Test" в body
+app.use("/static", function(request, response){
+    response.json({
+        header: "Hello",
+        body: "Octagon NodeJS Test"
+    });
+});
+
+//dynamic возвращает json с результатом (a*b*c)/3, если a, b, c существуют
+app.get('/dynamic', (request, response) => {
+    const { a, b, c } = request.query;
+    if (isNumber(a) && isNumber(b) && isNumber(c)) {
+		//parseFloat - cast в тип float 
+        const aNum = parseFloat(a);
+        const bNum = parseFloat(b);
+        const cNum = parseFloat(c);
+        
+        const result = (aNum * bNum * cNum) / 3;
+        
+        response.json({
+            header: "Calculated",
+            body: result.toString()
+        });
+    } else {
+		//Если a или b или c - NaN или после trim length = 0
+        response.json({
+            header: "Error"
+        });
+    }
+});
+
+//Прослушивание порта port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
