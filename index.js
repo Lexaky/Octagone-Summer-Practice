9c8b75e9772263c19a8449603dfb98024fbc67c0
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-07-19 15:58:20 +1000

    Переработана логика отправки первого сообщения от бота новому пользователю

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const token = '7345012579:AAEpRCqweK2FLpRMfILGIf4Y9geDkpGdlHw';
 // Создание нового объекта типа TelegramBot
 const bot = new TelegramBot(token, {polling: true});
 
+const alreadyGreetUsers = {};
 
-//При вводе текста /start бот отправит сообщение "Привет, октагон!"
+// При вводе любого текста вначале диалога бот отправит сообщение "Привет, октагон!"
 bot.onText(/\/*/, (msg) => {
   const chatId = msg.chat.id;
-  const message = "Привет, октагон!";
 
-  bot.sendMessage(chatId, message);
+  if (!alreadyGreetUsers[chatId]) {
+    const message = "Привет, октагон!";
+    bot.sendMessage(chatId, message);
+    alreadyGreetUsers[chatId] = true;
+  }
+  //Иначе - пользователю уже было отправлено приветствие
+});
+
+bot.on('polling_error', (error) => {
+  console.error('polling_error', error);
 });
