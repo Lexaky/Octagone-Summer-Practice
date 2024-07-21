7126a41c74e39c5438f4e649141bbf0da8a7da3e
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-07-21 16:47:56 +1000

    Добавлен модуль axios для получения HTTP ответов от API screenshotapi, добавлены команды для получения qr кода текста и скриншота сайта

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const mysql = require("mysql2");
+const axios = require('axios');
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -17,14 +18,17 @@ connection.connect(function(err){
 });
 // Мой тг токен
 const token = '7345012579:AAEpRCqweK2FLpRMfILGIf4Y9geDkpGdlHw';
-
+// Токен для api сервиса со скриншотами сайтов
+const SCREENSHOTAPI_KEY = '6BE42G4-F6T4YWN-NBRJZCK-F7DEVFN';
 // Создание нового объекта типа TelegramBot
 const bot = new TelegramBot(token, {polling: true});
 const helpCommands = ["/site - отправляет в чат ссылку на сайт октагона", 
 					  "/creator - отправляет в чат ФИО разработчика",
 					  "/randomItem - возвращает случайный предмет",
 					  "/deleteItem [id] - удаляет предмет из БД по ID",
-					  "/getItemByID [id] - возвращает предмет из БД по ID"];
+					  "/getItemByID [id] - возвращает предмет из БД по ID",
+					  "!qr [text] - возвращает QR-код текста text",
+					  "!webscr [link] - отправляет изображение сайта с ссылкой link"];
 					  
 //Функция для отправки текста конкретному chatId
 function tell(id, message)
@@ -110,6 +114,25 @@ bot.onText(/\/getItemByID (.+)/, (msg, match) => {
 			tell(msg.chat.id, message);
 		}
 	});
+});
+
+bot.onText(/^\!qr/, function(msg) {
+  var userId = msg.from.id;
+  var data = msg.text.substring(3).trim();
+  var imageqr = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=" + data;
+bot.sendMessage(msg.chat.id, "[✏️](" + imageqr + ")QR код для сообщения: " + data,{parse_mode : "Markdown"});
+});
+
+bot.onText(/^\!webscr/, async (msg) => {
+  var url = msg.text.substring(8).trim();
+  var apiUrl = `https://shot.screenshotapi.net/screenshot?token=${SCREENSHOTAPI_KEY}&url=${encodeURIComponent(url)}&width=1280&height=720&full_page=true`;
+   try {
+    const response = await axios.get(apiUrl);
+    const imageUrl = response.data.screenshot;
+    bot.sendPhoto(msg.chat.id, imageUrl, {caption: `Картинка сайта: ${url}`});
+  } catch (error) {
+    bot.sendMessage(msg.chat.id, `Произошла ошибка при получении изображения сайта: ${error.message}`);
+  }
 });
 
 bot.on('polling_error', (error) => {
